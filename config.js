0c6001eec90afc2b0f188eb0ba3f004f2102755b
ChiaSyuan <chiasyuan@ChiaSyuandeMacBook-Air.local>
2026-06-16 20:11:28 +0800

    設定 GAS 密碼驗證網址

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
  * 設定一次之後就不需要再改這個檔案了。
  * 之後修改密碼請到 GAS 的「專案設定 → 指令碼屬性」修改。
  */
-const GAS_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycby_S3LxBGngxhzqUYu0iVVjBuiwt3i-8B8SlhrGxfqyNYhdvnz6cSD7aA41eia2umrQ/exec';
